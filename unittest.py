22a1abf5673af0308b141bd08dbd8140b2d596bf
ponnethali <112630760+ponnethali@users.noreply.github.com>
2022-09-06 10:10:05 +0530

    Update unittest.py

--- unittest.py
+++ unittest.py
@@ -1,12 +1,10 @@
 def test_register_user(self):
-        //response = self.client.post('/auth/register', data={
-        //    'username': 'alice',
-        //    'email': 'alice@example.com',
-        //    'password': 'foo',
-        //    'password2': 'foo',
-        //}, follow_redirects=True)
-        //assert response.status_code == 200
-        //assert response.request.path == '/auth/login' # redirected to login
+        #response = self.client.post('/auth/register', data={
+        #    'username': 'ali',
+        #    'password': 'f123',
+        #}, follow_redirects=True)
+        #assert response.status_code == 200
+        #assert response.request.path == '/auth/login' # redirected to login
 
         # login with new user
         response = self.client.post('/auth/login', data={
